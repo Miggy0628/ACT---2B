76d9d17e4ae057fd948e64649cbec71e19e2ac9d
CL1-PC18 <CL1-PC18@DESKTOP-502UDB1>
2025-07-18 09:16:40 +0800

    ACT - 2B

---
+++ src/main/java/com/mycompany/valencia/Valencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.valencia;
+
+/**
+ *
+ * @author CL1-PC18
+ */
+public class Valencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
